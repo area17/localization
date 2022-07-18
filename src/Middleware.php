8533dd3cfe3efd4def18a99b4db71438ed0f62d4
Antonio Ribeiro <antonio@area17.com>
2022-07-18 14:21:07 +0200

    Allow JSON to be responded

--- src/Middleware.php
+++ src/Middleware.php
@@ -5,11 +5,12 @@ namespace A17\Localization;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 
 class Middleware
 {
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         Localization::setRequest($request);
 
